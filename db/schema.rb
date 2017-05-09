2836c7817acada6e280e74675e27ee0b0daa1cc8
caward12 <ward.colleen.a@gmail.com>
2017-05-08 18:03:08 -0600

    change screen name to nickname in user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508223414) do
+ActiveRecord::Schema.define(version: 20170508235640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "screen_name"
+    t.string "nickname"
     t.string "uid"
     t.string "name"
     t.string "oauth_token"
---
+++ spec/features/user_logs_in_with_github_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "User logs in with github" do
+  it "they can log in with github" do
+    Capybara.app = Apicurious::Application
+
+    visit "/"
+
+    assert_equal 200, page.status_code
+    click_link "Login with Github"
+    assert_equal "/dashboard", current_path
+    assert page.has_link?("Logout")
+  end
+end
